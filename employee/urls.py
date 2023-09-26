453d34243da5f5976b71328e1be679d63323c076
tennitien <ntctien.forwork@gmail.com>
2023-09-26 21:45:08 +0700

    err-cryto

---
+++ employee/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'employee'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('them-moi/', views.add, name='add'),
+    path('cap-nhat/<int:pk>/', views.update, name='update'),
+    path('xoa/<int:pk>/', views.delete, name='delete'),
+]
--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import NhanVien
+from .forms import AddEmployeeForm
 # Create your views here.
+
+
+# @login_required(login_url=)
+def home(request):
+    objs = NhanVien.objects.all()
+    return render(request, 'employee/home.html', {'objs': objs})
+
+
+def add(request, ):
+    form = AddEmployeeForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Record Has Been Updated!")
+        return redirect('employee:home')
+    return render(request, 'employee/add.html', {'form': form})
+
+
+def update(request, pk):
+    current_employee = NhanVien.objects.get(id=pk)
+    form = AddEmployeeForm(request.POST or None, instance=current_employee)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Record Has Been Updated!")
+        return redirect('employee:home')
+    return render(request, 'employee/update.html', {'form': form})
+
+
+def delete(request, pk):
+    employee_del = NhanVien.objects.get(id=pk)
+    employee_del.delete()
+    messages.success(request, "Xóa nhân viên thành công")
+    return redirect('employee:home')
